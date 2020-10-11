eb147768423824849ecb8fcb07722080b2f89ea0
Tobi Adeyemi <oadeyemi@teamapt.com>
2020-10-11 17:16:31 +0100

    - update migration file - update controller to use new model values - add root route

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,27 @@
 class EntriesController < ApplicationController
+  def index
+    @new_entry = Entry.new
+    @all_entries = Entry.order(created_at: :desc).all
+  end
+
+  def create
+    @new_entry = Entry.new(entry_params)
+    if @new_entry.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def entry_params
+    params.require(:entry).permit(
+      :email, :estimated_pop, :radio_stations, :tv_stations,
+      :other_comms, :popular_unions, :literacy_level,
+      :agent_name, :agent_phone, :daily_withdraw_value,
+      :daily_deposit_count, :interest_level, :no_of_locations,
+      :location_serial_no, :location_platform, :location_est, :location_geotag,
+      :alajo_name, :alajo_phone, :alajo_prime_location, :alajo_no_of_locations,
+      :alajo_union, :alajo_population, :alajo_avg_age, :alajo_avg_customer,
+      :alajo_avg_daily_contr, :alajo_avg_acc_bal
+    )
+  end
 end
--- db/migrate/20201011005748_create_entries.rb
+++ db/migrate/20201011005748_create_entries.rb
@@ -7,6 +7,30 @@ class CreateEntries < ActiveRecord::Migration[6.0]
       t.string :tv_stations
       t.string :other_comms
       t.string :popular_unions
+      t.string :literacy_level
+
+      t.string :agent_name
+      t.string :agent_phone
+      t.decimal :daily_withdraw_value
+      t.integer :daily_deposit_count
+      t.string :interest_level
+      t.integer :no_of_locations
+
+      t.string :location_serial_no
+      t.string :location_platform
+      t.string :location_est
+      t.string :location_geotag
+
+      t.string :alajo_name
+      t.string :alajo_phone
+      t.string :alajo_prime_location
+      t.integer :alajo_no_of_locations
+      t.string :alajo_union
+      t.string :alajo_population
+      t.string :alajo_avg_age
+      t.string :alajo_avg_customer
+      t.string :alajo_avg_daily_contr
+      t.string :alajo_avg_acc_bal
       t.timestamps
     end
   end
