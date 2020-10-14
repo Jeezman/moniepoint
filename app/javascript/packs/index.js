37f05508d2579a0575a5207baf8f1696324d8691
Tobi Adeyemi <oadeyemi@teamapt.com>
2020-10-14 04:19:27 +0100

    cleanup

--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -1,10 +1,1 @@
 console.log('Hello from index.js')
-
-var locationform = document.getElementById("location");
-
-function addNewLocationForm() {
-    console.log('add new form clicked 000')
-    console.log('form is ', locationform)
-}
-
-window.addNewLocationForm = addNewLocationForm
