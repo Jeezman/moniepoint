e9d045dc815f2b771d1cddcc279decdead7138b1
Tobi Adeyemi <oadeyemi@teamapt.com>
2020-10-14 04:14:40 +0100

    duplicate field on add button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,27 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require('jquery')
 
+// $(document).ready(function() {
+//     $('[data-js-hide-link]').click(function(event){
+//       alert('You clicked the Hide link');
+//       event.preventDefault(); 
+//     });
+//   }
+
+$(function() {
+    console.log('jquery don load')
+    // $( "p" ).text( "The DOM is now loaded and can be manipulated." );
+
+    function cloneDiv() {
+        let item = $('#location-fields');
+        let wrap = $('#location-wrap-content')
+        // console.log('item is ', item)
+        item.clone().appendTo(wrap)
+    }
+    $('#location-btn').on("click", cloneDiv)
+});
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
